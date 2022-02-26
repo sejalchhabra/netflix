b3dc5e1b5f18a52792fdb63427be9dd20bfcc5b9
Sejal chhabra <97884777+sejalchhabra@users.noreply.github.com>
2022-02-26 17:25:29 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,7 @@
+let api_key="95aae2d61923df6f9171bb46bb4ed214";
+let img_url = "https://image.tmdb.org/t/p/w500/";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http="https://api.themoviedb.org/3/movie";
+let movie_video_http="https://api.themoviedb.org/3/movie/{movie_id}/videos?";
